9da7c254de98c0ccabfb2e7db862cf047398deae
unknown <josealfredo2108@outlook.com>
2024-12-02 18:33:46 -0300

    adiciona link em recomendados

--- src/components/featured-items/featured-items.js
+++ src/components/featured-items/featured-items.js
@@ -4,6 +4,7 @@ const FeaturedContent = ({ imgSrc, label, labelstyle, title, price }) => {
     
     return (
         <div className="feature-item">
+            <a href="/produto">
             <div className="feature-item__img">
                 <img src={imgSrc} />
                 <span className="feature-item__img-tag"  style={{...labelstyle }}>{label}</span>
@@ -16,6 +17,7 @@ const FeaturedContent = ({ imgSrc, label, labelstyle, title, price }) => {
                     <p>{price}</p>
                 </div>
             </div>
+            </a>
         </div>
     )
 }
