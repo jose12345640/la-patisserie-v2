20f7e51fe39e12c22a28098944bdd05e8cfe1a98
unknown <josealfredo2108@outlook.com>
2024-12-02 19:18:50 -0300

    adiciona link login

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -15,8 +15,19 @@ function Header() {
                 </ul>
             </div>
             <div className="header__user">
-                <div className="header__user-obj">
-                    <a className="" href="/carrinho"><img src="https://i.ibb.co/k2YH5Xy/Vector-1.png" /></a>
+                <div className="header__user-acessibility">
+                    <div className="acessibility-btn">
+                        <span>A+</span>
+                    </div>
+                    <div className="acessibility-btn">
+                        <span>a-</span>
+                    </div>
+                </div>
+                <div className="header__user-cart">
+                    <a href="/carrinho"><img src="https://i.ibb.co/k2YH5Xy/Vector-1.png" /></a>
+                </div>
+                <div className="header__user-profile">
+                    <a href="/login"></a>
                 </div>
             </div>
         </header>
