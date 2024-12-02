f9b72e4e845afaf31724f42db22a2865a6c59f5f
unknown <josealfredo2108@outlook.com>
2024-12-02 18:25:17 -0300

    adiciona recomendados

--- src/pages/introduction/intro.js
+++ src/pages/introduction/intro.js
@@ -1,8 +1,44 @@
 import "./intro.scss"
 import FeaturedContent from "../../components/featured-items/featured-items"
 
-function Intro() {
-    
+const produtosRecomendados = [
+    {
+        imgSrc: "https://i.ibb.co/WgYzpvJ/image-7.png",
+        label: "Tortas",
+        labelstyle: {
+            backgroundColor: '#FFD5D5', 
+            border: '1px solid #FF9EBB', 
+            color: '#FF6B97'
+        },
+        title: "Torta Floresta Negra",
+        price: "R$ 122,00"
+    },
+    {
+        imgSrc: "https://i.ibb.co/hsjbLtw/image-6.png",
+        label: "Chocolateria",
+        labelstyle: {
+            backgroundColor: '#D5FFE1', 
+            border: '1px solid #28E544', 
+            color: '#28E544'
+        },
+        title: "Truffes Pistachio",
+        price: "R$ 63,00"
+    },
+    {
+        imgSrc: "https://i.ibb.co/G2rpYD8/image-8-1.png",
+        label: "Bolos",
+        labelstyle: {
+            backgroundColor: '#FDFFD5', 
+            border: '1px solid #E1D600', 
+            color: '#E1D600'
+        },
+        title: "Cream Tart Number",
+        price: "R$ 92,00"
+    }
+];
+
+
+function Intro() {    
     return (
         <div className="intro">
             <div className="banner__content">
@@ -51,39 +87,16 @@ function Intro() {
             <div className="featured__content">
                 <h2>Delicias em Destaque</h2>
                 <div className="featured__content-items">
-                    <FeaturedContent
-                        imgSrc="https://i.ibb.co/WgYzpvJ/image-7.png"
-                        label="Tortas"
-                        labelstyle={{
-                            backgroundColor: '#FFD5D5', 
-                            border: '1px solid #FF9EBB', 
-                            color: '#FF6B97'
-                        }}
-                        title="Torta Floresta Negra"
-                        price="R$ 122,00"
-                    />
-                    <FeaturedContent
-                        imgSrc="https://i.ibb.co/hsjbLtw/image-6.png"
-                        label="Chocolateria"
-                        labelstyle={{
-                            backgroundColor: '#D5FFE1', 
-                            border: '1px solid #28E544', 
-                            color: '#28E544'
-                        }}
-                        title="Truffes Pistachio"
-                        price="R$ 63,00"
-                    />
-                    <FeaturedContent
-                        imgSrc="https://i.ibb.co/G2rpYD8/image-8-1.png"
-                        label="Bolos"
-                        labelstyle={{
-                            backgroundColor: '#FDFFD5', 
-                            border: '1px solid #E1D600', 
-                            color: '#E1D600'
-                        }}
-                        title="Cream Tart Number"
-                        price="R$ 92,00"
-                    />
+                        {produtosRecomendados.map((produtoRecomendado, index) => (
+                            <FeaturedContent
+                                key={index}
+                                imgSrc={produtoRecomendado.imgSrc}
+                                label={produtoRecomendado.label}
+                                labelstyle={produtoRecomendado.labelstyle}
+                                title={produtoRecomendado.title}
+                                price={produtoRecomendado.price}
+                            />
+                        ))}
                 </div>
             </div>
         </div>
--- src/pages/product/product.js
+++ src/pages/product/product.js
@@ -1,5 +1,7 @@
 import "./product.scss"
 import ProductSection from "../../components/productsection/product_section"
+import FeaturedContent from "../../components/featured-items/featured-items"
+
 
 
 const produtos = [
@@ -8,6 +10,42 @@ const produtos = [
         nameProduto: "Truffe Citron",
         descriptionProduto: "A clássica e famosa Floresta Negra tem como base Massa Sablée de Cacau, recheada com Geleia Artesanal de Cereja, Ganache Meio Amargo, finalizada com creme de chocolate, chantilly de nata, geleia de cereja, raspas de chocolate meio amargo e cerejas frescas.",
         priceProduto: "R$ 92,00"
+    }
+];
+
+const produtosRecomendados = [
+    {
+        imgSrc: "https://i.ibb.co/WgYzpvJ/image-7.png",
+        label: "Tortas",
+        labelstyle: {
+            backgroundColor: '#FFD5D5', 
+            border: '1px solid #FF9EBB', 
+            color: '#FF6B97'
+        },
+        title: "Torta Floresta Negra",
+        price: "R$ 122,00"
+    },
+    {
+        imgSrc: "https://i.ibb.co/hsjbLtw/image-6.png",
+        label: "Chocolateria",
+        labelstyle: {
+            backgroundColor: '#D5FFE1', 
+            border: '1px solid #28E544', 
+            color: '#28E544'
+        },
+        title: "Truffes Pistachio",
+        price: "R$ 63,00"
+    },
+    {
+        imgSrc: "https://i.ibb.co/G2rpYD8/image-8-1.png",
+        label: "Bolos",
+        labelstyle: {
+            backgroundColor: '#FDFFD5', 
+            border: '1px solid #E1D600', 
+            color: '#E1D600'
+        },
+        title: "Cream Tart Number",
+        price: "R$ 92,00"
     }
 ];
 
@@ -23,6 +61,21 @@ function Product() {
                         priceProduto={produto.priceProduto}
                     />
                 ))}
+                <div className="product__recomendation">
+                    <h2>Você Também Pode Gostar</h2>
+                    <div className="product__recomendation-items">
+                        {produtosRecomendados.map((produtoRecomendado, index) => (
+                            <FeaturedContent
+                                key={index}
+                                imgSrc={produtoRecomendado.imgSrc}
+                                label={produtoRecomendado.label}
+                                labelstyle={produtoRecomendado.labelstyle}
+                                title={produtoRecomendado.title}
+                                price={produtoRecomendado.price}
+                            />
+                        ))}
+                    </div>
+                </div>
         </div>
     )
 }
